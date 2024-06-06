ac8e18b6b692fca2ec286309f4eb0f573e5cd532
Merylcheng <163097111+Merylcheng@users.noreply.github.com>
2024-06-06 23:00:48 +0800

    style: featured sitters

--- src/components/Landing/Featured.jsx
+++ src/components/Landing/Featured.jsx
@@ -22,28 +22,48 @@ function Featured() {
   }, []);
 
   return (
-    <div>
-      <h1>Featured Sitters</h1>
-      <ul>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-semibold text-indigo-600 text-center mb-8">
+        Featured Sitters
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {sitters.map((sitter) => (
-          <li key={sitter._id}>
-            <h2>{sitter.title}</h2>
-            <p>{sitter.name}</p>
-            <p>{sitter.location}</p>
-            <p>{sitter.bio}</p>
-            <p>{sitter.experience}</p>
-            <p>Charges: ${sitter.charges}</p>
-            <p>Pet: {sitter.pet}</p>
-            <p>Pet Size: {sitter.petSize}</p>
-            {sitter.imageUrl && <img src={sitter.imageUrl} alt={sitter.name} />}
-            <Link to={`/sitters/${sitter._id}`}>
-              <button>View Details</button>
-            </Link>
-          </li>
+          <div
+            key={sitter._id}
+            className="bg-white shadow-md rounded-lg overflow-hidden"
+          >
+            {sitter.imageUrl && (
+              <img
+                src={sitter.imageUrl}
+                alt={sitter.name}
+                className="w-full h-48 object-cover"
+              />
+            )}
+            <div className="p-4">
+              <h2 className="text-xl font-bold mb-2">{sitter.title}</h2>
+              <p className="text-xl font-bold mb-2">{sitter.name}</p>
+              <p className="text-gray-700 mb-2">{sitter.location}</p>
+              <p className="text-gray-700 mb-2">{sitter.bio}</p>
+              <p className="text-gray-700 mb-2">{sitter.experience}</p>
+              <p className="text-gray-700 mb-2">Charges: ${sitter.charges}</p>
+              <p className="text-gray-700 mb-2">Pet: {sitter.pet}</p>
+              <p className="text-gray-700 mb-2">Pet Size: {sitter.petSize}</p>
+              <Link to={`/sitters/${sitter._id}`}>
+                <button className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition duration-200">
+                  View Details
+                </button>
+              </Link>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
 
 export default Featured;
+
+{
+  /* <p className="text-lg font-semibold text-gray-700 mb-2">
+{sitter.name} */
+}
