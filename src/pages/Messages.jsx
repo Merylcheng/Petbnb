ae9dc6734d76ebadbffb841e6467a8e7eb439783
Merylcheng <163097111+Merylcheng@users.noreply.github.com>
2024-06-11 16:27:36 +0800

    style: chatbox

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -69,22 +69,38 @@ const Messages = () => {
   };
 
   return (
-    <div>
-      <ul>
-        {messages.map((message) => (
-          <li key={message._id}>
-            <strong>{message.sender.name}:</strong> {message.messageContent}
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={sendMessage}>
-        <input
-          type="text"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: `url('/images/pet4.jpg')` }}
+    >
+      <div className="max-w-md w-full bg-indigo bg-opacity-90 shadow-lg rounded-lg p-4">
+        <h1 className="text-2xl font-bold text-center mb-4 text-indigo-600">
+          ChatBox
+        </h1>
+
+        <ul className="space-y-4">
+          {messages.map((message) => (
+            <li key={message._id} className="p-3 bg-gray-100 rounded-md">
+              <strong className="text-blue-500">{message.sender.name}:</strong>{" "}
+              {message.messageContent}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={sendMessage} className="mt-4 flex">
+          <input
+            type="text"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            className="flex-1 p-2 border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500"
+          />
+          <button
+            type="submit"
+            className="bg-indigo-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600 focus:outline-none"
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
